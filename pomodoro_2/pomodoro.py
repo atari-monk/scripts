3975cd5811d713618e26c4ae4d0735c9f38e3742
atari-monk <atari.monk1@gmail.com>
2024-02-26 22:54:46 +0100

    upd popup pomodoro

--- pomodoro_2/pomodoro.py
+++ pomodoro_2/pomodoro.py
@@ -7,10 +7,12 @@ def show_popup():
     root.withdraw()
     root.attributes('-topmost', 1)
     messagebox.showinfo("Pomodoro", "Time to take a break!")
+    print("Popup displayed on the screen.")
 
 def pomodoro_timer(minutes):
+    print("Starting pomodoro timer.")
     time.sleep(minutes * 60)
     show_popup()
 
 if __name__ == "__main__":
-    pomodoro_timer(0.05)
+    pomodoro_timer(25)
