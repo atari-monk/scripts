70af0c7680ef318ffc8c3e4a73a3cded95ed8d67
atari-monk <atari.monk1@gmail.com>
2024-03-24 21:39:09 +0100

    refactor

--- mouse/automate.py
+++ mouse/automate.py
@@ -1,14 +1,24 @@
 import time
 import pyautogui
 import json
+import keyboard
 
 def execute_mouse_commands_with_repeats(commands, repeats):
     for _ in range(repeats):
         for command in commands:
+            if keyboard.is_pressed('esc'):
+                print("Execution stopped by user.")
+                return
             if command["command"] == "move":
                 pyautogui.moveTo(command["x"], command["y"], duration=command["duration"])
             elif command["command"] == "click":
-                pyautogui.click(button=command["button"])
+                pyautogui.click(button=command["button"], duration=command["duration"])
+            elif command["command"] == "ctrl_v":
+                pyautogui.hotkey('ctrl', 'v')
+                time.sleep(command["duration"])
+            elif command["command"] == "enter":
+                pyautogui.press('enter')
+                time.sleep(command["duration"])
             time.sleep(command["duration"])
 
 try:
@@ -17,7 +27,7 @@ except ValueError:
     print("Invalid input. Please enter a valid number.")
     repeats = 0
 
-with open('mouse_commands.json', 'r') as file:
+with open('links_list.json', 'r') as file:
     mouse_commands = json.load(file)
 
 execute_mouse_commands_with_repeats(mouse_commands, repeats)
