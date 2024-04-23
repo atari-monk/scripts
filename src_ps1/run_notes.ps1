f1c059ff0adbc086b25be62c22be2f08e4a88dee
atari-monk <atari.monk1@gmail.com>
2024-04-23 12:14:28 +0200

    add script

---
+++ src_ps1/run_notes.ps1
@@ -0,0 +1,27 @@
+$clientPath = "C:\atari-monk\code\notes_app\editor\build\index.html"
+$readerPath = "C:\atari-monk\code\notes_app\reader\build\index.html"
+$serverPath = "C:\atari-monk\code\notes_app\server\build\server.js"
+
+if (Test-Path $clientPath -PathType Leaf) {
+    Start-Process $clientPath
+}
+else {
+    Write-Host "The client file does not exist: $clientPath"
+}
+
+if (Test-Path $readerPath -PathType Leaf) {
+    Start-Process $readerPath
+}
+else {
+    Write-Host "The reader file does not exist: $readerPath"
+}
+
+if (Test-Path $serverPath -PathType Leaf) {
+    node $serverPath | ForEach-Object {
+        $_
+        $host.ui.RawUI.WindowTitle = 'Notes Server'
+    }
+}
+else {
+    Write-Host "The server file does not exist: $serverPath"
+}
