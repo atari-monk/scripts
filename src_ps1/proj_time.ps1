0be6660ff5d2253cd1c69a9f09a9027385c03438
atari-monk <atari.monk1@gmail.com>
2024-04-30 17:33:56 +0200

    fix timespan problem

--- src_ps1/proj_time.ps1
+++ src_ps1/proj_time.ps1
@@ -1,17 +1,19 @@
 # Read JSON data from file
-$jsonData = Get-Content -Raw -Path "../data/proj_time_json/03_2024_test.json" | ConvertFrom-Json
+$jsonData = Get-Content -Raw -Path "../data/proj_time_json/test.json" | ConvertFrom-Json
 
 # Initialize variables to store sums
-$totalSum = [timespan]::Zero
+$totalHours = 0
+$totalMinutes = 0
+
 $projectSums = @{}
 
 # Output file path
-$outputFilePath = "../data/proj_time_stats/03_2024_test.txt"  # Specify your desired output file path here
+$outputFilePath = "../data/proj_time_stats/test.txt"  # Specify your desired output file path here
 
 # Loop through each ID
 foreach ($obj in $jsonData) {
-    $objSum = [timespan]::Zero
-    $objProjectSums = @{}
+    $objHours = 0
+    $objMinutes = 0
 
     # Loop through each time entry for the ID
     foreach ($timeEntry in $obj.times) {
@@ -19,39 +21,54 @@ foreach ($obj in $jsonData) {
         $startTime = [datetime]::ParseExact($timeEntry.start, 'HH:mm', $null)
         $endTime = [datetime]::ParseExact($timeEntry.end, 'HH:mm', $null)
         
+        # Adjust end time if it's before start time (crosses midnight)
         if ($endTime -lt $startTime) {
-            # Add 24 hours to end time
             $endTime = $endTime.AddDays(1)
         }
-
+        
         $duration = $endTime - $startTime
 
+        # Calculate duration in hours and minutes
+        $hours = $duration.Hours
+        $minutes = $duration.Minutes
+
         # Update total sum
-        $totalSum += $duration
+        $totalHours += $hours
+        $totalMinutes += $minutes
 
         # Update project sums
-        $objProjectSums[$timeEntry.proj] += $duration
-        $projectSums[$timeEntry.proj] += $duration
+        $proj = $timeEntry.proj
+        if (-not $projectSums.ContainsKey($proj)) {
+            $projectSums[$proj] = [pscustomobject]@{
+                Hours = $hours
+                Minutes = $minutes
+            }
+        } else {
+            $projectSums[$proj].Hours += $hours
+            $projectSums[$proj].Minutes += $minutes
+        }
+
+        # Check if minutes overflow to hours
+        if ($totalMinutes -ge 60) {
+            $totalHours += 1
+            $totalMinutes -= 60
+        }
     }
 
     # Output sum for the current ID
     $output = "ID: $($obj.id)`r`n"
-    foreach ($projSum in $objProjectSums.GetEnumerator()) {
+    foreach ($projSum in $projectSums.GetEnumerator()) {
         $projDuration = $projSum.Value
         $output += "Project $($projSum.Name) Sum: $($projDuration.Hours) hours $($projDuration.Minutes) minutes`r`n"
-        $objSum += $projDuration  # Update $objSum with project duration
     }
-    $output += "Total Sum: $($objSum.Hours) hours $($objSum.Minutes) minutes`r`n`r`n"
+    $output += "Total Sum: $totalHours hours $totalMinutes minutes`r`n`r`n"
 
     # Write output to file
     $output | Out-File -FilePath $outputFilePath -Append -Encoding utf8
-
-    # Update ID sum
-    $objSum = $objProjectSums.Values | Measure-Object -Property TotalSeconds -Sum | Select-Object -ExpandProperty Sum
 }
 
 # Output sum for all IDs
-$output = "Total Sum of All IDs: $($totalSum.Hours) hours $($totalSum.Minutes) minutes`r`n"
+$output = "Total Sum of All IDs: $totalHours hours $totalMinutes minutes`r`n"
 $output += "Project Sums for All IDs:`r`n"
 foreach ($projSum in $projectSums.GetEnumerator()) {
     $projDuration = $projSum.Value
