be703d9b0c05d11eb254dc6a9408ad84a331c84b
atari-monk <atari.monk1@gmail.com>
2025-07-29 11:30:45 +0200

    Fix Blog Index

--- scripts/info.py
+++ scripts/info.py
@@ -1,5 +1,5 @@
 def main():
-    commands = ["info", "focus_drone", "tree_to_clipboard", "blog_index", "text_adventure"]
+    commands = ["info", "focus_drone", "tree_to_clipboard", "blog_index_h", "blog_index_ul", "text_adventure"]
 
     for command in commands:
         print(command)
